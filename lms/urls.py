bc5f64acb884d1afd67109066d20b7c5f8e3bd5b
Theo Gicquel <theo.gicquel@weuplearning.com>
2025-11-12 04:47:53 +0100

    add dummy api

--- lms/urls.py
+++ lms/urls.py
@@ -1053,3 +1053,9 @@ urlpatterns += [
 urlpatterns += [
     path('api/notifications/', include('openedx.core.djangoapps.notifications.urls')),
 ]
+
+
+
+urlpatterns += [
+    path('wul_apps/', include('lms.djangoapps.wul_apps.urls')),
+]
